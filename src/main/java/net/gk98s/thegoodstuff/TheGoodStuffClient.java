af4c46ae1d66ae67269f8afd276471cdc03866d9
gk98s <imnotgk98+git@gmail.com>
2023-03-09 20:44:35 +0300

    far from release

---
+++ src/main/java/net/gk98s/thegoodstuff/TheGoodStuffClient.java
@@ -0,0 +1,10 @@
+package net.gk98s.thegoodstuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheGoodStuffClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
--- src/main/java/net/gk98s/thegoodstuff/mixin/ExampleMixin.java
+++ src/main/java/net/gk98s/thegoodstuff/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.gk98s.thegoodstuff.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.gk98s.thegoodstuff.TheGoodStuff;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		TheGoodStuff.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
